6acbbdeceb86907f953404295480d7b45aca13e4
nick <nsgodshall@gmail.com>
2021-07-23 00:46:01 -0700

    fixed bugs in value counting

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <cassert>
 
 #include "game.h"
 
-int main() {
+void testHandVal(){
+  card c;
+  c.suit = 'S';
+  c.val = 'J';
 
-  std::srand(std::time(NULL));
-  // deck longDeck;
-  // longDeck.dump();
-  // longDeck.shuffleDeck();
-  // std::cout << "================= " << std::endl;
-  // longDeck.dump();
-  game g1(1,0,0,1);
-  g1.play();
+  player p("debug");
+  p.addCard(c);
+
+  c.val = 'A';
+  p.addCard(c);
+
+  assert(getHandVal(p.getHand()) == 0);
+  c.val = '2';
+  p.addCard(c);
+  assert(getHandVal(p.getHand()) == 13);
+
+}
+
+int main() {
+  testHandVal();
 }
