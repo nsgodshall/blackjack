53e45cb063e1ce868ea5d4589e3894d83696f2d8
nick <nsgodshall@gmail.com>
2022-11-20 15:48:32 -0800

    reviving project, adding comments

--- src/card.h
+++ src/card.h
@@ -1,6 +1,14 @@
 #ifndef CARD_INCLUDED
 #define CARD_INCLUDED
 
+
+// structure defining a given card. 
+// SUITS:
+//  (D)iamonds
+//  (S)pades
+//  (C)lubs
+//  (H)earts
+
 struct card {
   char val;
   char suit;
--- src/deck.h
+++ src/deck.h
@@ -10,15 +10,25 @@
 
 class deck {
 public:
+  // Initialize a deck with a shoe size of 1
   deck();
+
+  // Initialize a deck with a shoe size of n
   deck(int shoeSize);
 
-  // accessors
+  // ACCESSORS
+  // return the amount of cards that still could be drawn in the deck
   int size() const { return m_nCards; }
 
-  // mutators
+  // MUTATORS
+
+  // Change the order of the cards in the deck randomly
   bool shuffleDeck();
+
+  // return a card to be played and remove that card from the deck
   bool drawCard(card &c);
+
+  // print all cards in the deck to screen, for debugging purposes
   void dump();
 
 private:
