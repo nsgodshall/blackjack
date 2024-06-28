2d0d65b2642e23bb55dd13f66aa6270a8beb0e8d
Nick <nsgodshall@gmail.com>
2024-06-28 15:13:27 -0700

    fixed the framework for hand class

--- src/hand.cpp
+++ src/hand.cpp
@@ -2,19 +2,23 @@
 #include "player.h"
 
 
-
-
 // ACCESSORS
     int hand::getNumCards() const{
-
+        ;
     }
-    std::vector<card *> hand::getCardsInHand() const;
-    card* hand::getLastCard() const;
-    int hand::getHandVal() const;
+    std::vector<card *> hand::getCardsInHand() const{
+        ;
+    }
+    card* hand::getLastCard() const{
+        ;
+    }
+    int hand::getHandVal() const{
+        ;
+    }
     
 // MUTATORS
     card* hand::addCardToHand(card* addedCard){
         m_hand.push_back(addedCard);
         return addedCard;
     }
-    int hand::clearHand();
+    int hand::clearHand(){;}
